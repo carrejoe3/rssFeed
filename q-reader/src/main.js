0462882641fc89d4f52078997cef61b3b158167e
Joe Carre <joe.carre@rfa.co.uk>
2018-07-04 11:49:38 +0100

    Inserted assets from tutorial

--- q-reader/src/main.js
+++ q-reader/src/main.js
@@ -1,15 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import element from 'element-ui'
 import router from './router'
+import store from './store'
 
+// Use plugins
+Vue.use(element)
+
+// Components
+import App from './App'
+
+// Configs
+Vue.config.devtools = true
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  render: h => h(App)
 })
--- q-reader/src/router/index.js
+++ q-reader/src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
+// Components
+import Dashboard from '@/components/Dashboard'
+import Channel from '@/components/Channel'
+import Favs from '@/components/Favs'
+
+// Use the router
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/channel/:index/:title',
+      name: 'Channel',
+      component: Channel,
+      props: true
+    },
+    {
+      path: '/favorites',
+      name: 'Favs',
+      component: Favs
     }
   ]
 })
